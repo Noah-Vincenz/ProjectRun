e9f96be97490ff9d709174da2c770f431040dac3
Rob Hanns <robert.hanns@kcl.ac.uk>
2017-02-13 22:00:16 +0000

    added panda assets and animation to waiting room.

---
+++ Assets/Scripts/PandaController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PandaController : MonoBehaviour {
+	SpriteRenderer spriteRenderer;
+	BoxCollider2D bc;
+	int click = 0; 
+
+	public Animator animator;
+	public GameObject speechBub;
+	public GameObject text;
+	public GameObject chair;
+	public GameObject Z;
+	public GameObject nextBtn;
+	public float spawnTime;
+	public float speed; 
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		bc = gameObject.GetComponent<BoxCollider2D>();
+		InvokeRepeating ("SpawnZ", 0, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnMouseDown()
+	{
+		switch(click){
+			case 0://wake up panda
+				CancelInvoke ();
+				Debug.Log ("Panda Click event-1");
+				animator.SetTrigger ("Wake");
+				++click;
+				break;
+			case 1://move to centre 
+				Debug.Log ("Panda Click event-2");
+			
+				moveToPos (new Vector3(1.5f,-3.00f,0.00f)); // move to centre 
+				chair.GetComponent<BoxCollider2D>().enabled = true;
+				++click;
+
+				break;
+			case 2: // show speech bubble 
+				Debug.Log ("Panda Click event-3");
+				speechBub.GetComponent<Renderer> ().enabled = true;
+				++click;
+				break;
+			case 3:// show text 
+				Debug.Log ("Panda Click event-4");
+				text.GetComponent<Renderer> ().enabled = true;
+				nextBtn.GetComponent<Renderer> ().enabled = true;
+				++click;
+				break;
+				
+			default : // default animation
+				animator.SetTrigger ("Attack");
+				break;
+
+		}
+	}
+	/**
+	 * move Gameobject to new position.
+	 * 
+	 **/
+	public void moveToPos(Vector3 newPos)
+	{
+		transform.position = Vector3.Lerp(transform.position, newPos, speed);
+	}
+	/**
+	 * Spawns Zs where panda sleeps
+	 **/
+	void SpawnZ()
+	{
+		var newZ = GameObject.Instantiate(Z);
+	}
+}
---
+++ Assets/Scripts/Zcontrol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zcontrol : MonoBehaviour {
+	
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+//		InvokeRepeating ("SpawnZ", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce (Vector2.up * 1);
+		if (transform.position.y > 10) {
+			Debug.Log ("trying to kill");
+			Destroy (this.gameObject);
+		}
+	}
+
+
+
+}
