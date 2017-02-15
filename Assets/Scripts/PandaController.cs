a672bedd471c71e7804981453e406b977e2af3f8
Rob Hanns <robert.hanns@kcl.ac.uk>
2017-02-15 14:27:40 +0000

    animated panda walk to centre

--- Assets/Scripts/PandaController.cs
+++ Assets/Scripts/PandaController.cs
@@ -6,6 +6,8 @@ public class PandaController : MonoBehaviour {
 	SpriteRenderer spriteRenderer;
 	BoxCollider2D bc;
 	int click = 0; 
+	bool moving = false;
+	Rigidbody2D rb;
 
 	public Animator animator;
 	public GameObject speechBub;
@@ -22,6 +24,7 @@ public class PandaController : MonoBehaviour {
 	{
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 		bc = gameObject.GetComponent<BoxCollider2D>();
+		rb = GetComponent<Rigidbody2D> ();
 		InvokeRepeating ("SpawnZ", 0, spawnTime);
 	}
 	
@@ -34,34 +37,34 @@ public class PandaController : MonoBehaviour {
 	void OnMouseDown()
 	{
 		switch(click){
-			case 0://wake up panda
-				CancelInvoke ();
-				Debug.Log ("Panda Click event-1");
-				animator.SetTrigger ("Wake");
-				++click;
-				break;
-			case 1://move to centre 
-				Debug.Log ("Panda Click event-2");
-			
-				moveToPos (new Vector3(1.5f,-3.00f,0.00f)); // move to centre 
-				chair.GetComponent<BoxCollider2D>().enabled = true;
-				++click;
-
-				break;
-			case 2: // show speech bubble 
-				Debug.Log ("Panda Click event-3");
-				speechBub.GetComponent<Renderer> ().enabled = true;
-				++click;
-				break;
-			case 3:// show text 
-				Debug.Log ("Panda Click event-4");
-				text.GetComponent<Renderer> ().enabled = true;
-				nextBtn.GetComponent<Renderer> ().enabled = true;
-				++click;
-				break;
+		case 0://wake up panda
+			CancelInvoke ();
+			Debug.Log ("Panda Click event-1");
+			animator.SetTrigger ("Wake");
+			rb.gravityScale = 1;
+			++click;
+			break;
+		case 1://move to centre 
+			Debug.Log ("Panda Click event-2");
+			animator.SetTrigger ("Walk");
+			moveToPos (new Vector3(1.5f,-3.00f,0.00f)); // move to centre 
+			chair.GetComponent<BoxCollider2D>().enabled = true;
+			++click;
+			break;
+		case 2: // show speech bubble 
+			Debug.Log ("Panda Click event-3");
+			speechBub.GetComponent<Renderer> ().enabled = true;
+			++click;
+			break;
+		case 3:// show text 
+			Debug.Log ("Panda Click event-4");
+			text.GetComponent<Renderer> ().enabled = true;
+			nextBtn.GetComponent<Renderer> ().enabled = true;
+			++click;
+			break;
 				
-			default : // default animation
-				animator.SetTrigger ("Attack");
+		default : // default animation
+			animator.SetTrigger ("Attack");
 				break;
 
 		}
@@ -72,7 +75,10 @@ public class PandaController : MonoBehaviour {
 	 **/
 	public void moveToPos(Vector3 newPos)
 	{
-		transform.position = Vector3.Lerp(transform.position, newPos, speed);
+
+		// need to animate
+		rb.AddForce(Vector2.right * speed);
+		//transform.position = Vector3.Lerp(transform.position, newPos, speed);
 	}
 	/**
 	 * Spawns Zs where panda sleeps
@@ -81,4 +87,17 @@ public class PandaController : MonoBehaviour {
 	{
 		var newZ = GameObject.Instantiate(Z);
 	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		Debug.Log("hit");
+	}
+
+	private IEnumerator WaitForAnimation (Animation animation)
+	{
+		do
+		{
+			yield return null;
+		} while (animation.isPlaying);
+	}
+
 }
