9c2c6c5274b26f0bde27581a1a609bc4029f29d3
Jonathan Mounty <jonathan.mounty@kcl.ac.uk>
2017-02-22 01:08:44 +0000

    Add demo panda animations and prefab

---
+++ Assets/Scripts/NewPandaAnimationDemo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewPandaAnimationDemo : MonoBehaviour
+{
+    public float speed;
+
+    Animator anim;
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", rb.velocity.x);
+
+        if (Input.GetKeyDown("a"))
+            rb.velocity = Vector2.left * speed;
+
+        if (Input.GetKeyUp("a"))
+            rb.velocity = new Vector2(0,0);
+
+        if (Input.GetKeyDown("d"))
+            rb.velocity = Vector2.right * speed;
+
+        if (Input.GetKeyUp("d"))
+            rb.velocity = new Vector2(0, 0);
+
+        if (Input.GetKey("w"))
+            anim.SetBool("Happy", true);
+
+        if (Input.GetKey("s"))
+            anim.SetBool("Happy", false);
+    }
+}
