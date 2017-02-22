32127ec005f84e56a0cb5e8f372921741b1d378c
Jonathan Mounty <jonathan.mounty@kcl.ac.uk>
2017-02-22 12:26:16 +0000

    Add wave animations, improve animation structure

--- Assets/Scripts/NewPandaAnimationDemo.cs
+++ Assets/Scripts/NewPandaAnimationDemo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NewPandaAnimationDemo : MonoBehaviour
 {
     public float speed;
+    public GameObject face;
 
     Animator anim;
     Rigidbody2D rb;
@@ -22,21 +23,41 @@ public class NewPandaAnimationDemo : MonoBehaviour
         anim.SetFloat("Speed", rb.velocity.x);
 
         if (Input.GetKeyDown("a"))
+        {
             rb.velocity = Vector2.left * speed;
+            face.GetComponent<Animator>().SetBool("Walking", true);
+        }
 
         if (Input.GetKeyUp("a"))
-            rb.velocity = new Vector2(0,0);
+        {
+            rb.velocity = new Vector2(0, 0);
+            face.GetComponent<Animator>().SetBool("Walking", false);
+        }
 
         if (Input.GetKeyDown("d"))
+        {
             rb.velocity = Vector2.right * speed;
+            face.GetComponent<Animator>().SetBool("Walking", true);
+        }
 
         if (Input.GetKeyUp("d"))
+        {
             rb.velocity = new Vector2(0, 0);
+            face.GetComponent<Animator>().SetBool("Walking", false);
+        }
 
         if (Input.GetKey("w"))
-            anim.SetBool("Happy", true);
+            face.GetComponent<Animator>().SetBool("Happy", true);
 
         if (Input.GetKey("s"))
-            anim.SetBool("Happy", false);
+            face.GetComponent<Animator>().SetBool("Happy", false);
+
+        if (Input.GetKeyDown("q")) {
+            anim.SetBool("IsWaving", true);
+        }
+
+        if (Input.GetKeyUp("q")) {
+            anim.SetBool("IsWaving", false);
+        }
     }
 }
