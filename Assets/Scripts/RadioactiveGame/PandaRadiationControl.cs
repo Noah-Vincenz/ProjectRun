971b8d1402a80c20e9b865ec1eeb91cb1dcac20c
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2017-02-26 12:08:20 +0000

    Mini Game working. Will be further improved. Radiation symbol prefab and bamboo prefab will have to be changed and morre visual effects will be added, such as a +5 appearing in the middle of the screen when the panda collides with a bamboo prefab.

---
+++ Assets/Scripts/RadioactiveGame/PandaRadiationControl.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PandaRadiationControl : MonoBehaviour {
+
+	public float speed;
+	public GameObject face;
+	public int count; 
+	public Text countText;
+	public Text winText;
+	public Text gameOverText;
+	public Text timerLabel;
+	private float timeLeft;
+
+	Animator anim;
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start()
+	{
+		timeLeft = 30.0f;
+		count = 0;
+		SetCountText();
+		winText.text = "";
+		gameOverText.text = "";
+		anim = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		anim.SetFloat ("Speed", rb.velocity.x);
+		//transform.position += new Vector3 (Input.GetAxis ("Horizontal"), 0, 0);
+	
+		if (Input.GetKeyDown ("a")) {
+			rb.velocity = Vector2.left * speed;
+			face.GetComponent<Animator> ().SetBool ("Walking", true);
+
+		}
+
+		if (Input.GetKeyUp ("a")) {
+			rb.velocity = new Vector2 (0, 0);
+			face.GetComponent<Animator> ().SetBool ("Walking", false);
+		}
+
+		if (Input.GetKeyDown ("d")) {
+			rb.velocity = Vector2.right * speed;
+			face.GetComponent<Animator> ().SetBool ("Walking", true);
+		}
+
+		if (Input.GetKeyUp ("d")) {
+			rb.velocity = new Vector2 (0, 0);
+			face.GetComponent<Animator> ().SetBool ("Walking", false);
+		}
+
+		if (Input.GetKey ("w"))
+			face.GetComponent<Animator> ().SetBool ("Happy", true);
+
+		if (Input.GetKey ("s"))
+			face.GetComponent<Animator> ().SetBool ("Happy", false);
+
+		timeLeft -= Time.deltaTime;
+		timerLabel.text = "Time Left: " + Mathf.Round(timeLeft) + " secs";
+		if(timeLeft < 0)
+		{
+			gameOverText.text = "Game Over. Try again!";
+			Time.timeScale = 0;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		count = count + 1;
+		Destroy(coll.gameObject);
+		SetCountText ();
+		if (coll.gameObject.tag == "Bamboo") {
+			timeLeft += 5;
+		}
+		else if (coll.gameObject.tag == "Cube") {
+			gameOverText.text = "Game Over. Try again!";
+			Time.timeScale = 0;
+		}
+
+			
+	}
+
+	void SetCountText() 
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 20) 
+		{
+			winText.text = "You win! Captured all radiation!";
+			face.GetComponent<Animator> ().SetBool ("Happy", true);
+			//anim.SetBool ("isWaving", true);
+			Time.timeScale = 0;
+		}
+	}
+
+}
---
+++ Assets/Scripts/RadioactiveGame/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour {
+
+	public float delayRays = 1f;
+	public float delayBamboos = 7f;
+	public float delayCubes = 4f;
+	public GameObject RadioActive;
+	public GameObject Bamboo;
+	public GameObject Cube;
+
+	void Start() {
+		InvokeRepeating ("Spawn1", delayRays, delayRays);
+		InvokeRepeating ("Spawn2", delayBamboos, delayBamboos);
+		InvokeRepeating ("Spawn3", delayCubes, delayCubes);
+	}
+		
+	void Spawn1() {
+		Instantiate (RadioActive, new Vector3 (Random.Range (-9, 9), 5, 0), Quaternion.identity);
+	}
+	void Spawn2() {
+		Instantiate (Bamboo, new Vector3 (Random.Range (-9, 9), 5, 0), Quaternion.identity);
+	}
+	void Spawn3() {
+		Instantiate (Cube, new Vector3 (Random.Range (-4, 4), 5, 0), Quaternion.identity);
+	}
+}
