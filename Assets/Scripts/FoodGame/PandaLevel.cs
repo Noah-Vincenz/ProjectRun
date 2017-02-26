8689ba795bdb109bc919eae9fb554fcd8fbc40ff
Sam Larsen-Disney <s.larsendisney@gmail.com>
2017-02-26 01:13:36 +0000

    panda can now move between the levels to a fixed position.

---
+++ Assets/Scripts/FoodGame/PandaLevel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PandaLevel : MonoBehaviour
+{
+	public float dest;
+	public float speed;
+	public bool checkKeys;
+	private int count;
+	//Rigidbody2D rb;
+	void Start(){
+		count = 0;
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		double posX = transform.position.y;
+		if (posX < dest) {
+			double updatedPos = posX + (speed/12);
+			transform.position = new Vector2(transform.position.x, (float) updatedPos);
+		} 
+		else if (posX > dest) {
+			double updatedPos = posX - (speed/12);
+			transform.position = new Vector2(transform.position.x, (float) updatedPos);
+		} 
+		if (checkKeys) {
+			if (Input.GetKey ("w")) {
+			
+				if (dest == 0) {
+					dest = 3;
+				}
+				if (dest == -3) {
+					dest = 0;
+				}
+			}
+			if (Input.GetKey ("s")) {
+				if (dest == 3) {
+					dest = 0;
+				}
+				if (dest == 0) {
+					dest = -3;
+				}
+
+			}
+			checkKeys = false;
+		} 
+		else {
+			if (count == 3) {
+				checkKeys = true;
+				count = 0;
+			} else {
+				count++;
+			}
+		}
+		
+	}
+}
