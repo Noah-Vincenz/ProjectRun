4ac9ecbfbeb796a5e17eb7617112d0e4f6cc62e5
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2017-02-28 23:10:58 +0000

    made some further improvements. Added two buttons to the game to restart / continue. NEXT: I will draw the radiation and bamboo symbols, get the buttons to work and only show up at the end of the game, and add some little extras.

---
+++ Assets/TryAgainButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TryAgainButton : MonoBehaviour {
+
+	void Start () {
+		Button btn = gameObject.GetComponent<Button>();
+		btn.GetComponentInChildren<Text>().text = "Try Again";
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
