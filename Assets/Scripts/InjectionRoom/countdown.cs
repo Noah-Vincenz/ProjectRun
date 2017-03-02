2820bd1ee1d9482481e72d4335d896ae2de8513d
Jay Wong <jay.wong@kcl.ac.uk>
2017-03-02 01:39:46 +0000

    Added scenes

---
+++ Assets/Scripts/InjectionRoom/countdown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class countdown : MonoBehaviour {
+
+    public Text time = null;
+    float timeLeft = 5;
+
+    void Update()
+    {
+        if (Mathf.Round(timeLeft) == 1)
+        {
+            SceneManager.LoadScene("InjectionGame");
+        }
+
+        timeLeft -= Time.deltaTime;
+        changeText();
+
+    }
+
+    void changeText()
+    {
+        time.text = "" + Mathf.Round(timeLeft);
+    }
+}
