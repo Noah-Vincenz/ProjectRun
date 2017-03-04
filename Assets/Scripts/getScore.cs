f086aab610d738fd28626c90b9801e8ba69fd83a
Jay Wong <jay.wong@kcl.ac.uk>
2017-03-04 20:06:51 +0000

    Added new files

---
+++ Assets/Scripts/getScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class getScore : MonoBehaviour {
+    public Text scoreText = null;
+	void Start () {
+		if (SceneManager.GetActiveScene().name == "InjectionEnd")
+        {
+            scoreText.text = "Your score is : " + ScoreKeeper.injectionScore;
+            ScoreKeeper.injectionScore = 0;
+        }
+	}
+
+}
