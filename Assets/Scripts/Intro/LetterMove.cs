9a2e4877c7c3f8d64e635bfbcc5dc32e78b31ca2
chloe workman <chloe.workman@kcl.ac.uk>
2017-03-05 00:56:31 +0000

    Intro scene added along with an envelope animation for procedure appointment. Need updated waiting room and speech bubble after merge with master

---
+++ Assets/Scripts/Intro/LetterMove.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class LetterMove : MonoBehaviour {
+
+    Animator anim;
+	SpriteRenderer sp;
+	public Sprite back;
+	private int clicks = 0;
+	public Sprite openLet;
+	public GameObject swapBottom;
+	public GameObject swapTop;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		anim.SetTrigger("Drop");
+		anim.SetBool("Dropped",true);
+		sp = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void ChangeSprite(){
+		sp.sprite = back;
+	}
+
+	void OnMouseDown(){
+		switch (clicks){
+		case 0:
+			anim.SetTrigger ("flip");
+			anim.SetBool ("flipped", true);
+			++clicks;
+			break;
+		//case 1:
+			//sp.sprite = openLet;
+			//++clicks;
+			//break;
+
+		 default:
+			break;
+		}
+    }
+
+	void swapobjects(){
+		swapBottom.GetComponent<SpriteRenderer> ().enabled=true;
+		swapTop.GetComponent<SpriteRenderer> ().enabled = true;
+		Destroy (this.gameObject);
+
+	}
+
+}
