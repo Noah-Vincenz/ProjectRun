c0662cfe755a4e906cbd7419431cc73f510e5f05
Hailey Pun <hei.pun@kcl.ac.uk>
2017-03-14 19:30:41 +0000

    Added moving bed scene

---
+++ Assets/Scripts/ScanningRoom/MoveBed.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBed : MonoBehaviour {
+
+	private bool dirRight;
+	public float speed;
+	public float leftPos;
+	public float rightPos;
+
+	// Use this for initialization
+	void Start () {
+
+		dirRight = true;
+		speed = 2.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (dirRight)
+			transform.Translate (Vector2.right * speed * Time.deltaTime);
+		else
+			transform.Translate (-Vector2.right * speed * Time.deltaTime);
+
+		if(transform.position.x >= rightPos) {
+			dirRight = false;
+		}
+
+		if(transform.position.x <= leftPos) {
+			dirRight = true;
+		}
+		
+	}
+}
