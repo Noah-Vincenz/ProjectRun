f166c951897b0b07ee598009c57089d0a73743d9
Jonathan Mounty <jonathan.mounty@kcl.ac.uk>
2017-03-16 17:11:33 +0000

    Add timer script, bug with fading

---
+++ Assets/Scripts/ClockLater/SceneTimer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneTimer : MonoBehaviour {
+	public string sceneToLoad;
+	public float waitTime = 4;
+	public GameObject background;
+
+	private float loadTime;
+	private float timeLeftForTransition;
+	private Material material;
+	private Color color;
+
+	// Use this for initialization
+	void Start () {
+		//set the transition time to the current time + waitTime seconds
+		loadTime = Time.time + waitTime;
+
+		//fade code
+		Material material1 = background.GetComponent<Renderer>().material;
+		Color color1 = material1.color;
+		background.GetComponent<Renderer> ().material.color = new Color (color1.r, color1.g, color1.b, color1.a -color1.a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > waitTime) {
+			material = background.GetComponent<Renderer>().material;
+			color = material.color;
+			timeLeftForTransition -= Time.deltaTime;
+			if (timeLeftForTransition <= 2) {
+				background.SetActive (true);
+				material.color = new Color (color.r, color.g, color.b, color.a + (1f * Time.deltaTime));
+			}
+
+			if (timeLeftForTransition <= 0) {
+				SceneManager.LoadScene (sceneToLoad);
+			}
+		}
+	}
+}
