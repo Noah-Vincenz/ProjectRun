45b4e59f865e484c38723102aa33f5aeee71a884
Jay Wong <jay.wong@kcl.ac.uk>
2017-03-20 19:44:38 +0000

    Made changes to cursor and text

---
+++ Assets/Scripts/AnestheticCream/cursor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePosNeed = new Vector3(mousePos.x, mousePos.y, mousePos.z);
+        mousePosNeed.z = 5f;
+        transform.position = mousePosNeed;
+    }
+}
