f181ea71f5cb6b9bfe44dab9189f122c4b2509d1
Hailey Pun <hei.pun@kcl.ac.uk>
2017-03-22 15:35:59 +0000

    Replaced PNG speech bubbles and added prompts

---
+++ Assets/Scripts/Intro/Transition.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Transition : MonoBehaviour {
+
+	public GameObject prompt;
+	bool interacted;
+	public float wait;
+
+
+	// Use this for initialization
+	void Start () {
+
+		interacted = false;
+		StartCoroutine ("prompt_time"); 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+			
+	}
+
+	void OnMouseDown(){
+
+		interacted = true; // stops prompt 
+		prompt.SetActive (false);
+		
+		SceneManager.LoadScene("WaitingRoom");
+
+	}
+
+	IEnumerator prompt_time()
+	{
+		yield return new WaitForSeconds(wait);
+		if (!(interacted)) {
+			prompt.SetActive (true);
+		}
+	}
+}
