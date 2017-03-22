baec8605bcebec50dcc014a7195e6d91d7e2681e
Iona <iona.ryder@kcl.ac.uk>
2017-03-22 14:03:57 +0000

    3 second and GO! added and prompt

--- Assets/FoodIntro.cs
+++ Assets/FoodIntro.cs
@@ -16,59 +16,33 @@ public class FoodIntro : MonoBehaviour {
 	public GameObject arrow;
 	public GameObject FoodInstructions;
 	public GameObject startBtn;
-	private bool firstClick = true;
 	public GameObject background;
-	float timeLeftforTransition=2;
-	private bool readyToTransition;
+	public GameObject prompt;
 
+
+	private bool firstClick = true;
+	private bool readyToTransition;
+	private bool interacted = false;
+
+	public float wait;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-		readyToTransition = false;
-		var material1 = background.GetComponent<Renderer>().material;
-		var color1 = material1.color;
-		//background.GetComponent<Renderer> ().material.color = new Color (color1.r, color1.g, color1.b, color1.a -color1.a);fz
+		StartCoroutine ("prompt_time");
 
 	}
-//
-//	void Clicked(){
-//		speechBub.GetComponent<Renderer> ().enabled = true;// renders speech bubble
-//		text1.GetComponent<Renderer> ().enabled = false;// renders text 1
-//		text2.GetComponent<Renderer> ().enabled = true;
-//		arrow.GetComponent<Renderer> ().enabled = true; //renders the arrow
-//	}
-//
-	
 	// Update is called once per frame
 	void Update () {
-		var material = background.GetComponent<Renderer>().material;
-		var color = material.color;
-		if (readyToTransition) {
-			Debug.Log ("ds");
-			background.SetActive (enabled);
-			material.color = new Color (color.r, color.g, color.b, color.a + (1f * Time.deltaTime));
-			timeLeftforTransition -= Time.deltaTime;
-		}
-
-		if (timeLeftforTransition <= 0) {
-			SceneManager.LoadScene ("FoodGame");
-		}
-
 		if (Input.GetMouseButtonDown(0) && click == 0){
 			Debug.Log("0 button clicked" + firstClick);
+			interacted = true; //stops prompt
+			prompt.SetActive (false); //removes the prompt 
 			speechBub.GetComponent<Renderer> ().enabled = true;// renders speech bubble
 			text1.GetComponent<Renderer> ().enabled = true;// renders text 2
 			arrow.GetComponent<Renderer> ().enabled = true; //renders the arrow
 			++click;
 		}
-//		(Input.GetMouseButtonDown (0) && click == 1) {
-//			Debug.Log ("Panda Click event 2");
-//			firstClick = false;
-//			Destroy (text1.gameObject);
-//			text2.GetComponent<Renderer> ().enabled = true;// renders text 2
-//			++click;
-//		}
 	}
 
 
@@ -76,14 +50,6 @@ public class FoodIntro : MonoBehaviour {
 
 		Debug.Log ("Clicks:" + click);
 		switch (click) {
-
-//		case 0: // show speech bubble 
-//			Debug.Log ("Panda Click event 1");
-//			speechBub.GetComponent<Renderer> ().enabled = true;// renders speech bubble
-//			text1.GetComponent<Renderer> ().enabled = true;// renders text 2
-//			arrow.GetComponent<Renderer> ().enabled = true; //renders the arrow
-//			++click;
-//			break;
 		case 1:// show text 
 			Debug.Log ("Panda Click event 2");
 //			firstClick = false;
@@ -102,20 +68,18 @@ public class FoodIntro : MonoBehaviour {
 			startBtn.GetComponent<StartBtnScript> ().enabled = true;
 			++click;
 			break;
-//
-//		default :
-//			speechBub.GetComponent<Renderer> ().enabled = true;// renders speech bubble
-//			break;
 
 		}
 
 
 	}
 
-	void TaskOnClick()
+	IEnumerator prompt_time()
 	{
-		Debug.Log ("d");
-		readyToTransition = true;
+		yield return new WaitForSeconds(wait);
+		if (!(interacted)) {
+			prompt.SetActive (true);
+		}
 	}
 
 }
