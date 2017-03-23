974b64bdb2ac3a4ed0c92ad3b7bd76f032664c6e
Nicolas Zuluaga <nicolas.zuluaga@kcl.ac.uk>
2017-03-23 22:47:40 +0000

    fixed bugs found in testing of webgl

--- Assets/Scripts/InjectionRoom/TargetCreationControl.cs
+++ Assets/Scripts/InjectionRoom/TargetCreationControl.cs
@@ -32,10 +32,10 @@ public class TargetCreationControl : MonoBehaviour {
 				print (whatColourTargt);
 
 				if (whatColourTargt == 1) {
-					Vector3 screenPosition = Camera.main.ScreenToWorldPoint (new Vector3 (Random.Range (350, 850), Random.Range (50, 400), ZvalueAsFloat));
+					Vector3 screenPosition = Camera.main.ScreenToWorldPoint (new Vector3 (Random.Range (300, 700), Random.Range (50, 400), ZvalueAsFloat));
 					redTargetIn=(GameObject) Instantiate(redTarget, screenPosition, redTarget.transform.rotation);
 				} else if (whatColourTargt == 0||whatColourTargt == 2||whatColourTargt == 3) {
-					Vector3 screenPosition = Camera.main.ScreenToWorldPoint (new Vector3 (Random.Range (350, 850), Random.Range (50, 400), ZvalueAsFloat));
+					Vector3 screenPosition = Camera.main.ScreenToWorldPoint (new Vector3 (Random.Range (300, 700), Random.Range (50, 400), ZvalueAsFloat));
 					greenTargetIn=(GameObject) Instantiate (greenTarget, screenPosition, redTarget.transform.rotation);
 				}
 
