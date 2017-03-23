0db26416ed7202c4ef1b3fc847f491f9a07ffe11
Robert <robert.hanns@kcl.ac.uk>
2017-03-23 21:28:49 +0000

    All the prompts in the world #PandaLYF

--- Assets/Scripts/Intro/TopTriangleMove.cs
+++ Assets/Scripts/Intro/TopTriangleMove.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TopTriangleMove : MonoBehaviour {
 
@@ -11,6 +12,11 @@ public class TopTriangleMove : MonoBehaviour {
 	public Sprite top;
     SpriteRenderer sp;
 	private AudioSource source;
+	bool clicked = false; 
+	public float wait;
+	public GameObject prompt;
+	public GameObject prompt2;
+	bool open = false;
 	// Use this for initialization
 	void Start () {
 		sp = GetComponent<SpriteRenderer>();
@@ -20,23 +26,26 @@ public class TopTriangleMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!frontLetter.activeInHierarchy && !open) {
+			source.Play ();
+			clicked = true;
+			anim.SetTrigger ("Open");
+			anim.SetBool ("Opened", true);
+			prompt.SetActive (false);
+			clicked = false;
+			Destroy (prompt.gameObject);
+			StartCoroutine ("prompt_time2");
+			open = true;
+		}
 	}
 		
 
 	void OnMouseDown(){
 		switch (clicks) {
+
 		case 0:
-			if (!frontLetter.activeInHierarchy) {
-				source.Play ();
-				anim.SetTrigger ("Open");
-				anim.SetBool ("Opened", true);
-				++clicks;
-				break;
-			}
-			break;
-		case 1:
 			if (anim.GetCurrentAnimatorStateInfo (0).IsName ("idletoptri")) {
+
 				SceneManager.LoadScene ("OpenedLetter");
 				++clicks;
 				break;
@@ -50,5 +59,14 @@ public class TopTriangleMove : MonoBehaviour {
 		sp.sprite = top;
 
 	}
-
+	/**
+	 * Enum to wait the given seconds argument before rendering the prompt
+	 */
+	IEnumerator prompt_time2()
+	{
+		yield return new WaitForSeconds(wait);
+		if (!(clicked)) {
+			prompt2.SetActive (true);
+		}
+	}
 }
