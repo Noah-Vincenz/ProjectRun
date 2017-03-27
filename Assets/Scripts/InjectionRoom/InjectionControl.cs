e60db7a55ba8bc70fa1fde7e26860f6f36ddc9b0
Nicolas Zuluaga <nicolas.zuluaga@kcl.ac.uk>
2017-03-27 03:28:08 +0100

    tidied injection scene scripts and fixed radiation bug

--- Assets/Scripts/InjectionRoom/InjectionControl.cs
+++ Assets/Scripts/InjectionRoom/InjectionControl.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/*
+ * This script is in charge of the animation for the injection character
+ */
 
 public class InjectionControl : MonoBehaviour {
-	public Animator anim;
-	public float xPos=1; //turned into 0.6 from object Inspector
-	public GameObject timerKeeper;
-	Timer timer;
+	
+	public Animator anim; //injection character animator
+
+	private float xPos=0.6f;
+
+	private  GameObject timerKeeper;
+	private Timer timer;
+
+
 	// Use this for initialization
 	void Start () {
 		timerKeeper = GameObject.Find ("Canvas/Timer").gameObject;
@@ -16,28 +24,23 @@ public class InjectionControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 
+
+		//If game isnt over, follow the cursor location
 		if (timer.getTime ()>0.5) {
 			Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			Vector3 mousePosNeed = new Vector3 (mousePos.x + xPos, mousePos.y + 1, mousePos.z);
 			mousePosNeed.z = 5f;
-			transform.position = mousePosNeed;
+			transform.position = mousePosNeed; //injection character location=mouse location
 
-			if (Input.GetMouseButtonUp (0)) {
+			if (Input.GetMouseButtonUp (0)) { //if mouse is clicked, animations are triggered
 				anim.SetTrigger ("Inject");
 				anim.SetTrigger ("refill");
-
 			}
-
 		} 
-		else if(timer.getTime () <= 0.5){
+
+		//If game is over, freeze location
+		else if(timer.getTime () <= 0.5)
 			transform.position = transform.position;
-		}
 	}
-
-
-			
-
-
-
 
 }
--- Assets/Scripts/InjectionRoom/countdown.cs
+++ Assets/Scripts/InjectionRoom/countdown.cs
@@ -4,35 +4,38 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+/*This script is in charge of creating the 321 counter in injection scene once start is clicked
+ */
 public class countdown : MonoBehaviour {
 
     public Text time = null;
-	public GameObject timeGaOb;
-    float timeLeft = 3;
+	public GameObject timeGameObj;//created to facilitate appearance of text since its UI
+    private float timeLeft = 3;//3 seconds
 	public AudioSource source;
 
 	void Start () {
-		timeGaOb.SetActive (true);
-		source.Play ();
+		timeGameObj.SetActive (true);//show timer
+		source.Play ();//play sound
 	}
 
     void Update()
     {
 		if (timeLeft <= 1&&timeLeft>0) {
 			timeLeft -= Time.deltaTime;
-			time.text = "GO!";
+			time.text = "GO!";//If it reaches 1 change text to Go and play
 		}
 
 		else if(timeLeft<=0)
-			SceneManager.LoadScene("InjectionGame");
+			SceneManager.LoadScene("InjectionGame");// load injection game
 		
         else
         {
             timeLeft -= Time.deltaTime;
-            changeText();
+            changeText();//change text to new time rounded
         }
     }
 
+	//Round time to nearest second
     void changeText()
     {
         time.text = "" + Mathf.Round(timeLeft);
